e81f9d647b175d1a6ab06bee2c2b5356be2ad7a9
Travis DeveloperTra <you@example.com>
2019-01-12 02:48:13 -0800

    uploading

--- auths/views.py
+++ auths/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
+from .models import User
 # Create your views here.
 
 def login_view(request):
@@ -21,7 +21,17 @@ def signup_view(request):
     if request.method == "POST":
         email = request.POST['email']
         password = request.POST['password']
-        cpassword = request.POST['cpassword']
+        firstname = request.POST['firstname']
+        lastname = ""
+        users = User.objects.filter(email__iexact=email).first()
+        if users:
+            return render(request, "auths/signup.html", {'error' : "Email is already existed!"})
+        user = User()
+        user.email = email
+        user.set_password(password)
+        user.first_name = firstname
+        user.last_name = lastname
+        user.save()
         print(email)
     return render(request, "auths/signup.html")
 
